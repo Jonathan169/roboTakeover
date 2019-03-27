057bbdc1098233c2cd040eb3d44300e9cc21ef3d
Jonathan Hernandez <jon.hernandez169@gmail.com>
2019-03-27 15:47:25 -0500

    Updates

---
+++ precache-manifest.108255755141525520c3d90ef75ab534.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "149f25efd10d78d0d036",
+    "url": "/roboTakeover/static/js/runtime~main.34189a11.js"
+  },
+  {
+    "revision": "a415d35bbc3efd9ddc0c",
+    "url": "/roboTakeover/static/js/main.2580ff21.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/roboTakeover/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "a415d35bbc3efd9ddc0c",
+    "url": "/roboTakeover/static/css/main.a956fcb5.chunk.css"
+  },
+  {
+    "revision": "403d492f771076262c00d57d4a9addec",
+    "url": "/roboTakeover/index.html"
+  }
+];
